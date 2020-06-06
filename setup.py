bf17b1d5d9f79d8330dfeecc996797ea33c673b6
mikhailsirenko <m.sirenko@tudelft.nl>
2020-06-06 14:42:02 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='What makes us move? Exploratory analysis of citizens mobility.',
-    author='Yap Jin Rui, Mikhail Sirenko & Trivik Verma',
+    description='',
+    author='',
     license='',
 )
